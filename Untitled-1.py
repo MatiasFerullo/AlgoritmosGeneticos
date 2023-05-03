29b0fc1decd91c7f7c2bea083922ec701ff9fbc0
MarcosLeones <87377508+MarcosLeones@users.noreply.github.com>
2023-05-03 16:52:59 -0300

    selección de torneo y mutación

--- Untitled-1.py
+++ Untitled-1.py
@@ -22,8 +22,33 @@ def ruleta(listFitness: list, popSelected: int) -> list:
         elegidos.append(choices(list, listFitness)[0])
     return elegidos
 
-def mutacion():
-    pass
+
+def torneo(listFitness : list, popSelected: int, torneo_size: int) ->list:
+    elegidos = [] 
+    for _ in range(popSelected): 
+        list = [j for j in range(len(listFitness))]
+        torneo = sample(list, torneo_size) #elige participantes del torneo
+        listFitness_torneo = [listFitness[i] for i in torneo]
+        elegidos.append(choices(torneo, listFitness_torneo)) #elige ganador del torneo con probabilidad basada en su fitness
+    flat_elegidos = []
+    for e in elegidos:
+        flat_elegidos.extend(e)
+    return flat_elegidos
+        
+       
+
+def mutacion(cromosoma : str, prob: float) -> list:
+    lista = list(cromosoma)
+    if random() < prob: #determina si se aplica la mutación o no basado en la probabilidad de mutación
+        punto = randint(1, len(lista) - 1) #determina el gen que se va a mutar
+        #Método de mutación: Invertida. Si es 1 pone 0 y si es 0 pone 1
+        if lista[punto] == 0:
+            lista[punto] = 1
+        else: 
+            lista[punto] = 0
+        cromosoma = str(lista)
+    return cromosoma 
+
 
 def crossover():
     pass
@@ -46,7 +71,13 @@ def main():
     funcs = fitness(objFuncs)
     print(funcs)
     print(sum(funcs))
+    print("Ruleta:")
     print(ruleta(funcs, 2))
+    print("Torneo:")
+    print(torneo(funcs, 2, 4))
+    mutacion(inputsBin[0], 0.99)
+
+
 
 
 if __name__ == '__main__':
